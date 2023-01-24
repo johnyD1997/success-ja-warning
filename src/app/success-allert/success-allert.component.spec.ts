31cf6cc51abccb85973f4659a58ad39b9b56bcdd
Ваня Дубинников <ivan.dubinnikov@gmail.com>
2023-01-24 10:50:02 +0200

    Create two components: succes and warning alerts

---
+++ src/app/success-allert/success-allert.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SuccessAllertComponent } from './success-allert.component';
+
+describe('SuccessAllertComponent', () => {
+  let component: SuccessAllertComponent;
+  let fixture: ComponentFixture<SuccessAllertComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SuccessAllertComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SuccessAllertComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
